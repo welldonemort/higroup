9507117db37e1f0723c07bacb78c47d366a4003c
Вильдан Хисамутдинов <welldonemort@gmail.com>
2025-09-18 18:46:33 +0500

    gpt rop mobile

--- pages/gpt/script.js
+++ pages/gpt/script.js
@@ -1,9 +1,12 @@
 const logoContent = document.getElementById("logo-content");
+const logoContentMobile = document.getElementById("logo-content-mobile");
 
 toggle.addEventListener("click", () => {
   if (document.body.classList.contains("dark-theme")) {
     logoContent.src = "../../assets/icons/logo_night.svg";
+    logoContent.src = "../../assets/icons/logo-mobile_night.svg";
   } else {
     logoContent.src = "../../assets/icons/logo.svg";
+    logoContent.src = "../../assets/icons/logo-mobile.svg";
   }
 });
